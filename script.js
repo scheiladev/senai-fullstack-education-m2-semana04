e2d742ecfd50117440152b8dd3d7305b36962585
Scheila Stihler <sstihler@gmail.com>
2024-05-28 21:19:53 -0300

    Resolução do exercício 04

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 // Exercício 01
-
 let numeros = [8, 34, 13, 7, 49, 22];
 
 let soma = numeros.reduce((total, atual) => {
@@ -9,7 +8,6 @@ let soma = numeros.reduce((total, atual) => {
 console.log(soma);
 
 // Exercício 02
-
 let pares = numeros.filter((valor) => {
   if (valor % 2 == 0) {
     return true;
@@ -21,9 +19,19 @@ let pares = numeros.filter((valor) => {
 console.log(pares);
 
 // Exercício 03
-
 let quadrados = numeros.map((valor) => {
   return valor * 2;
 });
 
 console.log(quadrados);
+
+// Exercício 04
+class Produto {
+  constructor(nome, preco, quantidade) {
+    this.nome = nome;
+    this.preco = preco;
+    this.quantidade = quantidade;
+  }
+}
+
+const produtoNovo = new Produto("Caneta Bic Azul", 1.5, 5);
